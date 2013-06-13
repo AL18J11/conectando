e3da5d83ff4da48cbb332e1741762fe5b0660e66
AL18J11 <salvadorgtrc@gmail.com>
2013-06-13 08:58:14 -0500

    Update acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function LoginConectar (U, P)
 {
-	datos= "usuario=" +U+ "&paswsword" +P;
+	datos= "usuario=" +U+ "&password" +P;
 	$.ajax({
 		type: "POST",
 		url: "192.168.1.195/Siio6/Log_in_m.php" ,
